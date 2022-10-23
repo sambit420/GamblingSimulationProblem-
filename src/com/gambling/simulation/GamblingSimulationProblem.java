4ef24210bd5d22346f45627177423aa464d49b5e
sambit420 <sambit2024@gmail.com>
2022-10-23 23:46:27 +0530

    Added : Use Case 1 - Gambler starting the game with initial stake

--- src/com/gambling/simulation/GamblingSimulationProblem.java
+++ src/com/gambling/simulation/GamblingSimulationProblem.java
@@ -1,15 +1,32 @@
+//This program is to display the Welcome message to Gambling Simulation Program.
+//Use Case 1 is that As a Gambler, would start with a stake of $100 every day and bet $1 every game.
+
 package com.gambling.simulation;
 
 public class GamblingSimulationProblem {
     //Default Constructor
     public GamblingSimulationProblem() {
         System.out.println("Welcome to Gambling Simulation Program");
+        System.out.println();
+        System.out.println("Gambler starts the game...");
     }
+
+    //Declaring constants
+    final int betMoney = 1;
+    final int stakePerDay = 100;
 
     public static void main(String[] args) {
 
         //Initialize the object.
         GamblingSimulationProblem gambling = new GamblingSimulationProblem();
         System.out.println();
+
+        //Declaring variables
+        int stake = 0;
+        stake = stake + gambling.stakePerDay;
+        System.out.println("Initial stake of Gambler is : "+gambling.stakePerDay);
+        System.out.println("Initial betting money in gambling is : "+gambling.betMoney);
+
+
 }
 }
