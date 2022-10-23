76c2f29b6323ec881b8554eff02be2bbadc47453
sambit420 <sambit2024@gmail.com>
2022-10-23 23:31:05 +0530

    Added : Display Welcome to Gambling Simulation Program

---
+++ src/com/gambling/simulation/GamblingSimulationProblem.java
@@ -0,0 +1,15 @@
+package com.gambling.simulation;
+
+public class GamblingSimulationProblem {
+    //Default Constructor
+    public GamblingSimulationProblem() {
+        System.out.println("Welcome to Gambling Simulation Program");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize the object.
+        GamblingSimulationProblem gambling = new GamblingSimulationProblem();
+        System.out.println();
+}
+}
