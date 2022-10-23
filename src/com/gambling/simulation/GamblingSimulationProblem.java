74134375be8a0c335333ef7ad6e94cec30113c1b
sambit420 <sambit2024@gmail.com>
2022-10-24 00:04:56 +0530

    A

--- src/com/gambling/simulation/GamblingSimulationProblem.java
+++ src/com/gambling/simulation/GamblingSimulationProblem.java
@@ -1,7 +1,10 @@
 //This program is to display the Welcome message to Gambling Simulation Program.
 //Use Case 1 is that As a Gambler, would start with a stake of $100 every day and bet $1 every game.
+//Use Case 2 is that As a Gambler make $1 bet so either win or loose $1.
 
 package com.gambling.simulation;
+
+import java.util.Random;
 
 public class GamblingSimulationProblem {
     //Default Constructor
@@ -20,13 +23,34 @@ public class GamblingSimulationProblem {
         //Initialize the object.
         GamblingSimulationProblem gambling = new GamblingSimulationProblem();
         System.out.println();
-
-        //Declaring variables
-        int stake = 0;
-        stake = stake + gambling.stakePerDay;
         System.out.println("Initial stake of Gambler is : "+gambling.stakePerDay);
         System.out.println("Initial betting money in gambling is : "+gambling.betMoney);
+        startsGambling(gambling);
 
+    }
 
-}
+    //Method for starting the game.
+    public static void startsGambling(GamblingSimulationProblem gambling) {
+        //Declaring variables
+        Random random = new Random();
+        int stake = 0;
+        stake = stake + gambling.stakePerDay;
+        int bet = random.nextInt(2);
+
+        //Checking randomly if win or loose.
+        switch (bet)
+        {
+            case 1 :
+                System.out.println("Gambler won the bet.");
+                stake = stake + gambling.betMoney;
+                break;
+            default :
+                System.out.println("Gambler lost the bet.");
+                stake = stake - gambling.betMoney;
+                break;
+        }
+
+        System.out.println("Amount remaining at Gambler is : "+stake);
+
+    }
 }
